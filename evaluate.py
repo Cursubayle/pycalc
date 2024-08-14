7cc78be275828b53d4b7a2b4ddb2bec2fd54aa92
kelkavinsky <79155681+kelkavinsky@users.noreply.github.com>
2024-08-14 22:02:18 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,38 +1,39 @@
+def postfix(val):
+    ops = {'+':2, '-':2, '/':1, '*':1, '^':0}
 
-ops = {'+':2, '-':2, '/':1, '*':1, '^':0}
+    INPUT = val
 
-INPUT = '5+5*5'
+    stack, OUTPUT, digit = [], [], False
 
-stack, OUTPUT, digit = [], [], False
+    for s in INPUT:
+        
+        if s in '0123456789':
+            if len(OUTPUT) == 0:
+                OUTPUT = [s] + OUTPUT
+            else:
+                if OUTPUT[0][-1] in '0123456789' and digit: OUTPUT[0] += s
+                else: OUTPUT = [s] + OUTPUT
+            digit = True
+        else: digit = False
+        
+        if s == '(':
+            stack = [s] + stack
+        
+        if s == ')':
+            while stack != [] and stack[0] != '(': OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
+            if stack != [] and stack[0] == '(': stack = stack[1:]
+        
+        if s in ops:
+            while stack != [] and stack[0] in ops and ops[s] >= ops[stack[0]]: OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
+            stack = [s] + stack
 
-for s in INPUT:
-    
-    if s in '0123456789':
-        if len(OUTPUT) == 0:
-            OUTPUT = [s] + OUTPUT
-        else:
-            if OUTPUT[0][-1] in '0123456789' and digit: OUTPUT[0] += s
-            else: OUTPUT = [s] + OUTPUT
-        digit = True
-    else: digit = False
-    
-    if s == '(':
-        stack = [s] + stack
-    
-    if s == ')':
-        while stack != [] and stack[0] != '(': OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
-        if stack != [] and stack[0] == '(': stack = stack[1:]
-    
-    if s in ops:
-        while stack != [] and stack[0] in ops and ops[s] >= ops[stack[0]]: OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
-        stack = [s] + stack
+    while stack != []: OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
 
-while stack != []: OUTPUT, stack = [stack[0]] + OUTPUT, stack[1:]
-
-print('инфиксная запись:\t%s' % (INPUT))
-print('постфиксная запись:\t%s' % ("".join(reversed(OUTPUT))))
-            
-
+    #print('инфиксная запись:\t%s' % (INPUT))
+    #print('постфиксная запись:\t%s' % ("".join(reversed(OUTPUT))))
+    return "".join(reversed(OUTPUT))
+                
+print(postfix("5+5"))
 
 # Python program to evaluate value of a postfix expression
 
@@ -94,7 +95,7 @@ class Evaluate:
 
 # Driver code
 if __name__ == '__main__':
-	exp = "".join(reversed(OUTPUT))
+	exp = postfix("5+5")
 	obj = Evaluate(len(exp))
 	
 	# Function call
